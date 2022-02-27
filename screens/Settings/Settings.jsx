84a183c1d854bf44d0a069dd62fb0201a81f4228
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2022-02-27 22:18:53 +0600

    added settings page

--- screens/Settings/Settings.jsx
+++ screens/Settings/Settings.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import ScreenContainer from '../../components/shared/ScreenContainer';
 
 function Settings() {
   return (
-    <View>
-      <Text>Setting</Text>
-    </View>
+    <ScreenContainer>
+      <Card>
+        <View style={styles.container}>
+          <Icon name="account-circle" />
+          <Text style={styles.textStyle}>Profile Information</Text>
+        </View>
+      </Card>
+      <Card>
+        <View style={styles.container}>
+          <Icon name="login" />
+          <Text style={styles.textStyle}>Security</Text>
+        </View>
+      </Card>
+    </ScreenContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  textStyle: {
+    marginLeft: 10,
+  },
+});
 
 export default Settings;
