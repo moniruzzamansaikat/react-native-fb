b8d7d5a5ef9c4f478e14ec39f7f79547e1790a42
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2022-02-28 15:05:44 +0600

    post menu options added

--- components/Post/PostOptionMenu.jsx
+++ components/Post/PostOptionMenu.jsx
@@ -1,17 +1,38 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import React from 'react';
-import { Overlay } from 'react-native-elements';
+import { Card, Divider, ListItem, Overlay } from 'react-native-elements';
 
 const PostOptionMenu = ({ visible, onBackdropPress }) => {
   return (
     <Overlay
       isVisible={visible}
       onBackdropPress={onBackdropPress}
-      style={{ flex: 1 }}
+      overlayStyle={{ width: '90%', borderRadius: 10 }}
     >
-      <Text>Hello</Text>
+      <TouchableOpacity style={styles.itemStyle}>
+        <Text style={styles.textStyle}>Comment</Text>
+      </TouchableOpacity>
+      <Divider color="black" />
+      <TouchableOpacity style={styles.itemStyle}>
+        <Text style={styles.textStyle}>Unfollow</Text>
+      </TouchableOpacity>
+      <Divider color="black" />
+      <TouchableOpacity style={styles.itemStyle}>
+        <Text style={styles.textStyle}>Block</Text>
+      </TouchableOpacity>
     </Overlay>
   );
 };
+
+const styles = StyleSheet.create({
+  itemStyle: {
+    paddingVertical: 10,
+  },
+
+  textStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
 
 export default PostOptionMenu;
